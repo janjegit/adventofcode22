4a8db5b8651ef4acf7288641d214e34c28f30821
Jan <gitjaheje@mailbox.org>
2022-12-08 00:59:19 +0100

    Tag 6 Teil 2 fertig

--- src/D05_SupplyStacks.hs
+++ src/D05_SupplyStacks.hs
@@ -1,52 +1,10 @@
 module D05_SupplyStacks where
 
-import Data.Maybe
 import Data.List.Split
 import qualified Data.Sequence as Seq
 
-data Stack a = Empty | Stack a (Stack a)
-    deriving Show
-
-top :: Stack a -> Maybe a
-top Empty = Nothing
-top (Stack a s) = Just a
-
-pop :: Stack a -> Stack a
-pop Empty = Empty
-pop (Stack a s) = s
-
-push :: a -> Stack a -> Stack a
-push a Empty = Stack a Empty
-push a s = Stack a s
-
-build :: [Char] -> Stack Char
-build xs = build' (reverse xs) Empty 
-    where
-        build' [] s = s
-        build' (y:ys) s = build' ys (push y s)
-
-buildCargoHold :: [[Char]] -> Seq.Seq (Stack Char)
-buildCargoHold lst = buildHold (reverse lst)
-    where
-        buildHold [] = Seq.Empty
-        buildHold (x:xs) = (Seq.|>) (buildHold xs) (build (reverse x))
-
-move :: Int -> Int -> Seq.Seq (Stack Char) -> Seq.Seq (Stack Char)
-move from to sequ = if crate == Nothing
-                        then sequ
-                   else place
-    where
-        crate  = top (Seq.index sequ from)
-        remove = Seq.update from (pop (Seq.index sequ from)) sequ
-        place  = Seq.update to (push (fromJust crate) (Seq.index sequ to)) remove
-
-work :: Seq.Seq (Stack Char) -> [(Int,Int,Int)] -> Seq.Seq (Stack Char)
-work sequ instrLst = work' sequ instrLst
-    where
-        work' s [] = s
-        work' s (x:xs) = work (instr sequ x) xs
-        instr s (0,_,_) = s
-        instr s (reps,from,to) = instr (move (from-1) (to-1) s) ((reps-1),from,to)
+cargohold :: [[Char]] -> Seq.Seq ([Char])
+cargohold lst = Seq.fromList lst
 
 readInstr :: [[Char]] -> [(Int,Int,Int)]
 readInstr lst = map toTri lst 
@@ -58,15 +16,33 @@ toTri rawInstruction = (getN 0, getN 1, getN 2)
         toInt x = read x :: Int
         getN n = toInt (head (drop n splitInstr))
 
-eachTop :: Seq.Seq (Stack Char) -> [Char]
-eachTop Seq.Empty = [] 
-eachTop sequ = (contents sequ) : (eachTop (Seq.drop 1 sequ))
+move :: Int -> (Int,Int,Int) -> Seq.Seq ([Char]) -> Seq.Seq ([Char])
+move _ (0,_,_) cargo = cargo
+move num (repeats,from,to) cargo = 
+    move num (repeats-1,from,to) (Seq.update nfrom start (Seq.update nto dest cargo)) 
     where
-        topCrateOf s = top (Seq.index s 0)
-        contents s = if (topCrateOf s) == Nothing
-                        then 'x'
-                     else fromJust (topCrateOf s)
+        nfrom = (from - 1)
+        nto = (to - 1)
+        stack i = Seq.index cargo i
+        dest  = (take num (stack (nfrom))) ++ (stack nto)
+        start = drop num (stack (nfrom))
 
+work :: [(Int,Int,Int)] -> Seq.Seq ([Char]) -> Seq.Seq ([Char])
+work [] cargo = cargo
+work instructions cargo = work (drop 1 instructions) (move 1 instruction cargo)
+    where instruction = head instructions
+
+workfast :: [(Int,Int,Int)] -> Seq.Seq ([Char]) -> Seq.Seq ([Char])
+workfast [] cargo = cargo
+workfast instructions cargo = workfast (drop 1 instructions) (move num changedRep cargo)
+    where 
+        instruction = (head instructions)
+        num = (\(x,_,_) -> x) instruction
+        changedRep = (\(_,y,z) -> (1,y,z)) instruction
+
+getTops :: Seq.Seq ([Char]) -> [Char]
+getTops Seq.Empty = ""
+getTops cargo = (head (Seq.index cargo 0)) : getTops (Seq.drop 1 cargo)
 
 path :: String
 path = "input/supply_stacks.txt"
@@ -76,12 +52,6 @@ solve = do
     finput <- readFile path
     let splitting = splitOn "\n\n" finput
     let instructions = readInstr (lines $ last splitting)
-    let cargo = buildCargoHold (lines $ head splitting)
-    print (eachTop (work cargo instructions))
-
-cargolist :: [[Char]]
-cargolist = ["NZ","DCM","P"]
-instrlist :: [(Int,Int,Int)]
-instrlist = [(1,2,1),(3,1,3),(2,2,1),(1,1,2)]
-rawInstrlist :: [[Char]]
-rawInstrlist = ["1,2,1","3,1,3","2,2,1","1,1,2"]
+    let cargo = cargohold (map reverse (lines $ head splitting))
+    print (getTops (work instructions cargo))
+    print (getTops (workfast instructions cargo))
