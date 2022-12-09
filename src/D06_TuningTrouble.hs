26c38f41e291da01661fce2516b2afba0f6086c8
Jan <gitjaheje@mailbox.org>
2022-12-09 11:54:41 +0100

    Tag 6 Teil 1 gelöst

--- src/D06_TuningTrouble.hs
+++ src/D06_TuningTrouble.hs
@@ -3,17 +3,30 @@ module D06_TuningTrouble where
 import Data.List
 
 path :: [Char]
-path = "input/tuning_trouble_exp.txt"
+path = "input/tuning_trouble.txt"
 
 solve :: IO ()
 solve = do
     uinput <- readFile path
-    print uinput
+    let bufferlst = lines uinput
+    let markers = map checkMarker bufferlst
+    print markers
 
 hasUnique :: [Char] -> Bool
 hasUnique xs = length xs == length (uniqueElems xs)
+    where
+        uniqueElems [] = []
+        uniqueElems (y:ys) = y:uniqueElems (filter ((/=) y) ys)
 
-uniqueElems :: [Char] -> [Char] 
-uniqueElems [] = []
-uniqueElems (x:xs) = x:uniqueElems (filter ((/=) x) xs)
->>>>>>> origin/main
+checkMarker :: [Char] -> Int
+checkMarker str = check 0 "" str
+    where 
+        check n _ "" = n
+        check n r (x:xs) | less && unique = check (n+1) (x:r) xs
+                         | eq && unique = (n+1)
+                         | otherwise = check (n+1) (init (x:r)) xs
+            where 
+                less   = length (x:r) < 4
+                eq     = length (x:r) == 4
+                unique = hasUnique (x:r) 
+
